973d0db808f5fe155b306b9b2b0e4614c2401268
Brace1000 <brianmoraro500@gmail.com>
2025-07-30 12:34:59 +0300

    fix:Update users table to match hash password

--- backend/database/migrations/0001_create_users_table.up.sql
+++ backend/database/migrations/0001_create_users_table.up.sql
@@ -1,14 +1,13 @@
--- Up Migration: Creates the users table
 CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     nickname TEXT,
     email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    date_of_birth TEXT NOT NULL,
-    avatar_path TEXT,
+   
+    password_hash TEXT NOT NULL, 
+    date_of_birth TEXT,
     about_me TEXT,
-    is_public INTEGER NOT NULL DEFAULT 1, 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    avatar_url TEXT,
+    profile_type TEXT DEFAULT 'public'
 );
