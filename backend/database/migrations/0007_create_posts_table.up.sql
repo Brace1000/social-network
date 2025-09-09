79179ff38cb4871fd18f57878000fed9adc600f7
shaokoth <shadrackomondi005@gmail.com>
2025-09-09 03:58:08 +0300

    chore: Update schema

--- backend/database/migrations/0007_create_posts_table.up.sql
+++ backend/database/migrations/0007_create_posts_table.up.sql
@@ -6,7 +6,6 @@ CREATE TABLE posts (
     privacy TEXT DEFAULT 'public' CHECK (privacy IN ('public', 'almost_private', 'private')),
     group_id INTEGER DEFAULT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
 );
